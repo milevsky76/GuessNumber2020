df299dc45a959bc22c9761c1a884dc0f24e2a643
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-05-02 20:02:10 +0300

    Чтение и сохранение базы из файла
    + Выбор игрока

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,40 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите логин : ");
+            string login = Console.ReadLine();
+
+            string path = "players.txt";
+
+            string[] str = null;
+            string[] str2;
+            int id = 0;
+            bool newPl = false;
+            if (File.Exists(path))
+            {
+                str = File.ReadAllLines(path);
+            }
+            //else
+            //{
+            //    using (StreamWriter sw = new StreamWriter(path, false))
+            //    {
+            //        sw.WriteLine(login + ":" + 0);
+            //    }
+            //}
+
+            User[] users = new User[str.Length];
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                users[i] = new User();
+                str2 = str[i].Split(':');
+
+                users[i].Login = str2[0];
+                users[i].Money = Convert.ToInt32(str2[1]);
+            }
+
+
+
             //Console.WriteLine($"Ты тамада. Сколько дашь попыток?");
 
             var consoleHelper = new ConsoleHelper();
@@ -20,12 +55,59 @@ namespace ConsoleApp1
                 MaxAttempts = 3,
                 Number = 42
             };
+
+
+
+
             var user = new User();
+
+            for (int i = 0; i < users.Length; i++)
+            {
+                if (users[i].Login == login)
+                {
+                    user = users[i];
+                    id = i;
+                    newPl = false;
+                    break;
+                }
+                else
+                {
+                    if (i + 1 == users.Length)
+                    {
+                        user = new User(login);
+                        newPl = true;
+                        break;
+                    }
+                    continue;
+                }
+            }
+            //
             game.StartGame(gameRule, user);
 
             Console.WriteLine($"User score: {user.Money}");
 
-            Console.ReadLine();
+            if (newPl)
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine(user.Login + ":" + user.Money);
+                }
+            }
+            else
+            {
+                str[id] = user.Login + ":" + user.Money;
+
+                using (StreamWriter sw = new StreamWriter(path, false))
+                {
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        sw.WriteLine(str[i]);
+                    }
+                }
+                
+            }
+
+            Console.ReadKey();            
         }
     }
 }
